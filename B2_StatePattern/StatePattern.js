05bfed7774d0afe29f2656f1cccc8fb0a369c133
Vignan Kurme <37151166+vignan37@users.noreply.github.com>
2024-12-05 13:55:06 +0530

    Update StatePattern.js

--- B2_StatePattern/StatePattern.js
+++ B2_StatePattern/StatePattern.js
@@ -1,1 +1,5 @@
+/* 
+Resources:
+16. Design Vending Machine | LLD of Vending Machine | State Design Pattern | LLD interview question :  https://www.youtube.com/watch?v=wOXs5Z_z0Ew&t=0s by Concept && Coding - by Shrayansh
 
+*/
